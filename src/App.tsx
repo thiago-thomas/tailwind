b3f5ea9eeff18f0ae4a64d58c2ba0bee8f625996
Thiago Thomas <thiago_rocha17@hotmail.com>
2024-10-01 01:56:28 +0100

    first tailwind commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-3xl font-bold">Hello Tailwind</h1>
+    </div>
+  )
+}
+
+export default App
+ 
